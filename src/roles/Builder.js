c540afdb85f14364ad1f1b89b4a8388e9d60f404
Ernesto Jiménez Villaseñor <ernjivi@gmail.com>
2025-04-05 10:42:47 -0600

    feat: builder implements target

--- src/roles/Builder.js
+++ src/roles/Builder.js
@@ -7,26 +7,41 @@ export default class Builder extends BaseRole {
 
     if(this.working && creep.store[RESOURCE_ENERGY] === 0) {
       this.working = false;
+      this.target = null;
       creep.say('🔄 harvest');
     }
 
     if(!this.working && creep.store.getFreeCapacity() === 0) {
       this.working = true;
+      this.target = null;
       creep.say('🚧 build');
     }
 
     if(this.working) {
-      const targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-      if(targets.length) {
-        if(creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+      // building phase
+      if (!this.target || this.target.progress == this.target.progressTotal) {
+        const targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+        if (targets.length > 0) {
+          this.target = targets[0];
+        }
+      }
+      if (this.target) {
+        if (creep.build(this.target) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(this.target, { visualizePathStyle: { stroke: '#ffffff' } });
         }
       }
     } else {
-      const sources = creep.room.find(FIND_SOURCES);
-      if(sources.length > 0) {
-        if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+      // harvesting phase
+      if(!this.target || this.target.store.getFreeCapacity(RESOURCE_ENERGY) === 0) {
+        // if no target or target is full, find a source
+        const sources = creep.room.find(FIND_SOURCES);
+        if(sources.length > 0) {
+          this.target = sources[0];
+        }
+      }
+      if(this.target) {
+        if(creep.harvest(this.target) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(this.target, { visualizePathStyle: { stroke: '#ffaa00' } });
         }
       }
     }
