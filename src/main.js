0f6bde4501084ee397f3547d57b120d9a8eab020
Ernesto Jiménez Villaseñor <ernjivi@gmail.com>
2025-04-05 10:42:09 -0600

    fix: clear memory for dead creeps

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import './prototypes';
 
 export const loop = () => {
+  // Clear memory of dead creeps
+  for (const name in Memory.creeps) {
+    if (!Game.creeps[name]) {
+      delete Memory.creeps[name];
+    }
+  }
+  
   for (const name in Game.creeps) {
     const creep = Game.creeps[name];
     creep.runRole();
