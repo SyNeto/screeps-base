98c24b89a4b4a8a8080555df021abc2eb920984c
Ernesto Jiménez Villaseñor <ernjivi@gmail.com>
2025-04-07 08:46:15 -0600

    fix: warn does is not a property of console

--- src/prototypes/index.js
+++ src/prototypes/index.js
@@ -4,7 +4,7 @@ Creep.prototype.runRole = function () {
   const role = this.memory.role;
   const RoleClass = Roles[role];
   if (!RoleClass) {
-    console.warn(`[ERR] Role ${role} not found for creep ${this.name}`);
+    console.log(`[ERR] Role ${role} not found for creep ${this.name}`);
     return;
   }
   const roleInstance = new RoleClass(this);
