aa4c8d67d0c635d5d08e1db5a54324e9ca93315f
Ernesto Jiménez Villaseñor <ernjivi@gmail.com>
2025-04-09 21:24:17 -0600

    feat: harvester bad implementation

--- src/roles/Harvester.js
+++ src/roles/Harvester.js
@@ -1,5 +1,17 @@
 import BaseRole from "./Base";
 
+/**
+ * To-Do:
+ * Refactor this class, this class suffers from issues that came
+ * from the refactoring stage of the BaseRole class (while making test).
+ * 
+ * * Move the states to a separate module
+ * * refactor transition states to functions to reuse them (or use a class)
+ * * Refactor Base class to manage room coordinates as valid targets
+ * * Reimplment the Long Range Harvester to use the target mechanism
+ *   to manage rooms transfers
+ */
+
 
 const HARVESTER_STATES = {
   harvesting: {
@@ -10,11 +22,16 @@ const HARVESTER_STATES = {
       return null;
     },
     run: (role) => {
-      const sources = role.creep.room.find(FIND_SOURCES);
-      if (sources.length > 0) {
-        const source = sources[1]; // To-Do: Refactor this to use the closest source
-        if (role.creep.harvest(source) === ERR_NOT_IN_RANGE) {
-          role.creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
+      if (!role.target || role.target.energy === 0) {
+        const sources = role.creep.room.find(FIND_SOURCES);
+        if (sources.length > 0) {
+          role.target = sources[1];
+        }
+      }
+      if (role.target) {
+        const isSource = role.target instanceof Source;
+        if (role.creep.harvest(role.target) === ERR_NOT_IN_RANGE) {
+          role.creep.moveTo(role.target, { visualizePathStyle: { stroke: '#ffaa00' } });
         }
       }
     }
@@ -27,14 +44,30 @@ const HARVESTER_STATES = {
       return null;
     },
     run: (role) => {
-      const targets = role.creep.room.find(FIND_MY_STRUCTURES, {
-        filter: structure =>
-          [STRUCTURE_SPAWN, STRUCTURE_EXTENSION, STRUCTURE_TOWER].includes(structure.structureType) &&
-          structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 
-      });
-      if (targets.length > 0) {
-        if (role.creep.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-          role.creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+      if (!role.target) {
+        const targets = role.creep.room.find(FIND_MY_STRUCTURES, {
+          filter: structure =>
+            [STRUCTURE_SPAWN, STRUCTURE_EXTENSION, STRUCTURE_TOWER].includes(structure.structureType) &&
+            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+        });
+        if (targets.length > 0) {
+          role.target = targets[0];
+        }
+      }
+      if (role.target && role.target.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+        if (role.creep.transfer(role.target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+          role.creep.moveTo(role.target, { visualizePathStyle: { stroke: '#ffffff' } });
+        }
+      } else {
+        role.creep.say('😕')
+        const targets = role.creep.room.find(FIND_MY_STRUCTURES, {
+          filter: structure =>
+            [STRUCTURE_SPAWN, STRUCTURE_EXTENSION, STRUCTURE_TOWER].includes(structure.structureType) &&
+            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+        });
+        role.creep.moveTo(Game.spawns.Spawn1);
+        if (targets.length > 0) {
+          role.target = targets[0];
         }
       }
     }
@@ -45,13 +78,14 @@ const HARVESTER_STATES = {
 export default class Harvester extends BaseRole {
 
   states = HARVESTER_STATES;
-  
+
   getInitialState() {
     return 'harvesting';
   }
 
   onEnterState(newState) {
     const { creep } = this;
-    creep.say(newState === 'delivering' ?  '📤 delivering' : '⛏️ harvesting');
+    super.onEnterState(this, newState);
+    creep.say(newState === 'delivering' ? '📤 delivering' : '⛏️ harvesting');
   }
 }
